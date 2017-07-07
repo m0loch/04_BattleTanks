59b097dcaeddfe72fa6e6e9b73a4cf76f1c276d6
m0loch <romeo.graifenberg@gmail.com>
2017-07-07 22:07:18 +0200

    Lezione 155: EPPUR SI MUOVE!

--- BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
@@ -5,12 +5,11 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	UE_LOG(LogTemp, Warning, TEXT("%s throttle = %f"), *GetName(), Throttle)
-
 	float NormalThrottle = FMath::ClampAngle(Throttle, -1.0, 1.0);
 
 	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
+
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
